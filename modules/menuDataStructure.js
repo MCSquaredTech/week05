f4ac17e47af288b06b550934d20a243cdd34f65c
Mark Christensen <mark@mcsquared.tech>
2023-05-03 20:40:33 -0600

    04 - Completed Main Menu

--- modules/menuDataStructure.js
+++ modules/menuDataStructure.js
@@ -1,13 +1,31 @@
 export let main = [{
-        name: "Create New Recipe",
+        name: "Create New Car",
         identifier: 'N',
-        callback: "createNew()",
+        callback: "NewCar",
         description: "Create a new recipe from scratch"
     },
     {   
-        name: "Get Recipe List",
-        identifier: 'G', 
-        callback: "getList()",
+        name: "Select Cars",
+        identifier: 'S', 
+        callback: "selectCar",
         description: "Returns all recipes from Database"
+    },
+    {
+        name: "Delete Car",
+        identifier: "D", 
+        callback: "deleteCar", 
+        description: "Deletes selected car from list"
+    },
+    {
+        name: "Display All Car", 
+        identifier: "A",
+        callback: "allCars",
+        description: "Show all cars in the list"
+    },
+    {
+        name: "Exit Program", 
+        identifier: "X", 
+        callback: "exitProgram", 
+        description: "What can I, Stop it! Really!"
     }
 ];
--- src/week05-code-assignment.js
+++ src/week05-code-assignment.js
@@ -1,25 +1,49 @@
 import { MenuItems } from '../classes/menuItems.js'; 
 import { main } from '../modules/menuDataStructure.js';
 
+
+
+
+
+let handleCallBack = (action)  => { 
+
+    switch (action) {
+        case 'createNew()':
+            createNewCar();
+            break;
+        case 'selectCar':
+            selectCar();
+            break;
+        case 'deleteCar':
+            deleteCar();
+            break;
+        case 'allCars':
+            allCars();
+            break;
+        case 'exitProgram':
+            exitProgram();
+            break;
+    }
+}
+
+function display(menu) { 
+    return prompt(menu);
+}
+
 let mainMenu = new MenuItems('main', 'top'); 
 mainMenu.addFromFile(main); 
 
-let selectedMenu = 'S';
+let selectedMenu = '!';
 let selectedMenuOptions = mainMenu;
-
 
 while (selectedMenu !== 'X') {
     selectedMenuOptions.getMenuItems().forEach(menu => {
 
-        if (selectedMenu === menu.identifier) { 
-            menu.callback;
+        if (selectedMenu.toUpperCase() === menu.identifier) { 
+            handleCallBack(menu.callback);
         }
     })
    selectedMenu = display(selectedMenuOptions.displayMenu());   
 };
 
-
-function display(menu) { 
-    return prompt(menu);
-}
 
